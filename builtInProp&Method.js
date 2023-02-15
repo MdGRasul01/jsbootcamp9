7ce1a74395ee6abd68ed72da8752dd9e2174952b
Md Golam Rasul <grasul.dev@gmail.com>
2023-02-15 21:45:14 +0600

    array element i.e, add,remove completed

---
+++ builtInProp&Method.js
@@ -0,0 +1,16 @@
+// Built In Propertise and method
+// Array iteration
+// adding element  at the end of array
+const arr = [2, 3, 4];
+arr.push(5);
+console.log(arr);
+// remove element from the end of the array
+arr.pop(); //last element will remove autometically
+//adding element at the beginning of the array
+arr.unshift(1);
+console.log(arr);
+//remove element from the beginning of the array
+arr.shift(); // that will remove the 1st element from the array
+console.log(arr);
+//how to find out the last element of the array
+console.log(arr[arr.length - 1]);
