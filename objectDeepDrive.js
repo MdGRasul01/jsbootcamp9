04b9a44efd8824256e56433c09697cdfe0d2202e
Md Golam Rasul <grasul.dev@gmail.com>
2023-05-07 12:35:46 +0600

    how to retrive data from object through loop and dynamically

--- objectDeepDrive.js
+++ objectDeepDrive.js
@@ -1,15 +1,40 @@
 // Object deep Drive.
+// const profile = {
+//   firstName: "Rasul",
+//   lastName: "Mokabber",
+//   email: "grasul.dev@gmail.com",
+//   fullName: function () {
+//     return profile.firstName + " " + profile.lastName;
+//   },
+// };
+//This 2 way we can retrive the object
+// console.log(profile.firstName);
+//or
+// console.log(profile["lastName"]);
+// the way we can retrive the function from object
+// console.log(profile.fullName());
+
+// how to use "this":
 const profile = {
   firstName: "Rasul",
   lastName: "Mokabber",
   email: "grasul.dev@gmail.com",
-  fullName: function () {
-    return profile.firstName + " " + profile.lastName;
+  //modern day how to use the fuction on object
+  fullName() {
+    //how to use "this"
+    return this.firstName + " " + this.lastName;
   },
 };
-//This 2 way we can retrive the object
-console.log(profile.firstName);
-//or
-console.log(profile["lastName"]);
-// the way we can retrive the function from object
 console.log(profile.fullName());
+
+//Dynamically accessed properties
+const myVar = "email";
+console.log(profile.myVar); // which is not the way of retrive data from object
+// the way we can access
+console.log(profile[myVar]);
+
+// the way we can retrive data from object through for loop
+// for object we need to use the "for in loop"
+for (let key in profile) {
+  console.log(profile[key]);
+}
